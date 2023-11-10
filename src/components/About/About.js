146c5c37ea29988eec790d8eb7165162062d426a
merve14 <merve.cetinkaya416@gmail.com>
2023-11-10 21:32:43 +0100

    refactoring

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,6 @@ function About() {
     "Before venturing into the realm of technology, I was a textile designer, infusing my creations with colors and patterns inspired by life itself. My background in design has given me a keen eye for aesthetics and a strong foundation in tools like Adobe Photoshop and Illustrator, which I leverage in my web design and CSS projects.",
     "Though my professional experience is rooted in the projects I undertook during my course, my enthusiasm for coding knows no bounds. I find immense joy in crafting visually appealing and user-friendly websites, where my passion for painting and design seamlessly merges with the world of programming. I believe in the power of creativity to transform ideas into interactive experiences. Every project I undertake is an opportunity to combine my artistic sensibilities with my technical expertise, resulting in engaging digital solutions. I am constantly exploring new technologies and trends, driven by the desire to enhance my skills and create innovative web experiences.",
     "Let's embark on a creative journey together, where art meets technology, and innovation knows no bounds. Feel free to explore my portfolio, and don't hesitate to reach out—I'm always open to exciting collaborations and opportunities. À bientôt!",
-    "I have become confident using the following technologies:",
   ];
 
   return (
